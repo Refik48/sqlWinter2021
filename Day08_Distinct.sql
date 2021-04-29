a38a274a026c435c91ecc55d239336cf29d08aa9
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-04-29 17:33:51 +0300

    Day08_Distinct

---
+++ Day08_Distinct.sql
@@ -0,0 +1,9 @@
+/*============================= DISTINCT =====================================
+    DISTINCT cumlecigi bir SORGU ifadesinde benzer olan satirlari filtrelemek
+    icin kullanilir. Dolayisiyla secilen sutun yada sutunlar icin benzersiz veri iceren satirlar olusturmaya yarar.
+    
+    SYNTAX
+    -------
+    SELECT DISTINCT sutun_adi1, sutun_adi2, column_3
+    FROM  tablo_adi;
+==============================================================================*/
